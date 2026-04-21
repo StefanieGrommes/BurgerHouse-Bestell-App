28fafe67e52a29edfe2ad8c93002a1d4dc0f4914
Stefanie Grommes <s.trossen@web.de>
2026-04-21 14:30:47 +0200

    js menu added

--- script.js
+++ script.js
@@ -1,8 +1,72 @@
 function init(){
-    renderHeadline();
-    renderMealComponents();
+    renderCategory();
 }
 
-function renderHeadline(){
-    
+
+function renderCategory() {
+    let categoryContent = document.getElementById("category_container");
+    categoryContent.innerHTML = "";
+
+    for (let indexCategory = 0; indexCategory < categoryDetails.length; indexCategory++) {
+        const category = categoryDetails[indexCategory];
+        showCategory(indexCategory,categoryContent);
+        };
+
+
+function showCategory(indexCategory,categoryContent){
+    categoryContent.innerHTML +=
+    `<div class="main_dishes_category_container category_orange_bg">
+                    <div class="category_img_container"><img src="${categoryDetails[indexCategory].source}" alt="${categoryDetails[indexCategory].title}" id="category_img"></div>
+                    <div id="main_dishes_category"><h2>${categoryDetails[indexCategory].title}</h2></div>  
+                </div>
+    <div class="main_dishes" id="main_dish_${indexCategory}">
+    </div>`         
+
+    renderMealComponents(indexCategory);
 }
+}
+
+function renderMealComponents(indexCategory){
+    let mainDish = document.getElementById(`main_dish_${indexCategory}`);
+    mainDish.innerHTML = "";
+    let meals =  categoryDetails[indexCategory].meals;
+
+        for (let indexMealComponent = 0; indexMealComponent < meals.length; indexMealComponent++) {
+        mainDish.innerHTML += showMealComponents(indexMealComponent,indexCategory);
+        }
+}
+
+function showMealComponents(indexMealComponent,indexCategory){
+    let meal = categoryDetails[indexCategory].meals[indexMealComponent];
+   return `<div class="menu_card">
+                <div class="meal_component">
+                    <div class="meal_img_container">
+                        <img src="${meal.source}" alt="${meal.dish}" id="meal_img">
+                    </div>
+                    <div class="meal_details">
+                        <div class="meal_name_and_description">
+                            <div><h3>${meal.dish}</h3></div>
+                            <div><p class="meal_description">${meal.description}</p></div>
+                        </div>
+                        <div class="price_and_order_btn">
+                            <div id="meal_price"><h3>${meal.price}€</h3></div>
+                            <button type="submit" onclick="addToBasket()"class="order_btn">Add to basket</button>
+                        </div>
+                    </div>       
+                </div>
+            </div>`
+}
+
+function addToBasket(){
+    const myBasket = document.getElementById("my_basket")
+    myBasket.showModal();
+    myBasket.classList.add("open_basket");
+    document.body.classList.add("no-scroll");
+
+    //open dialogue classlist add 
+    //style: btn clicked verändert sich zu added 1
+
+
+}
+
+       
--- scripts /array.js
+++ scripts /array.js
@@ -1,90 +1,91 @@
-let dishesArray = [
-    {
-        "headline" : "Burger & Sandwiches",
-        "meal component" : [
+let burgerAndSandwiches = [
+   
             {"dish":  "Veggie mushroom black burger",
-            "decription": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-            "price": "16,90€"
+            "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "price": "16,90",
+            "source": "./assets/img/veggie_mushroom_black_burger.svg"
             },
             {
             "dish": "All meat burger",
-            "decription": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-            "price": "15,90€"
+            "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "price": "15,90",
+            "source": "./assets/img/all_meat_burger.svg"
             },
             {
             "dish": "Beef red burger",
-            "decription": "Beef, Cheese, Tomatoes, Lettuce, Onion",
-            "price": "14,90€"
+            "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "price": "14,90",
+            "source": "./assets/img/beef_red_burger.svg"
             },
             {
             "dish": "Blg chicken burger",
-            "decription": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-            "price": "13,90€"
+            "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+            "price": "13,90",
+            "source": "./assets/img/blg_chicken_burger.svg"
             }
-        
-        ]
-    },
-    {
-        "headline" : "Pizza (30cm)",
-        "meal component" : [
+]
+
+let pizza = [
             {"dish":  "Pizza Margherita",
-            "decription": "Tomato Sauce, Mozzarella",
-            "price": "11,90€"
+            "description": "Tomato Sauce, Mozzarella",
+            "price": "11,90",
+            "source": "./assets/img/pizza_margherita.svg"
             },
             {
             "dish": "Pizza Chorizo",
-            "decription": "Tomato slices, Mozzarella, Chorizo",
-            "price": "13,90€"
+            "description": "Tomato slices, Mozzarella, Chorizo",
+            "price": "13,90",
+            "source": "./assets/img/pizza_chorizo.svg"
             },
             {
             "dish": "Funghi",
-            "decription": "Red onion, Olives, Botton Mushrooms, Mozzarella",
-            "price": "12,90€"
+            "description": "Red onion, Olives, Botton Mushrooms, Mozzarella",
+            "price": "12,90",
+            "source": "./assets/img/pizza_funghi.svg"
             },
             {
             "dish": "Quattro Formaggi with Chicken",
-            "decription": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-            "price": "15,90€"
+            "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+            "price": "15,90",
+            "source": "./assets/img/pizza_quattro_fromaggi.svg"
             }
-        ]
-    },
-    {
-        "headline" : "Salad",
-        "meal component" : [
+]
+    
+let salad = [
             {"dish":  "Warm beef arugula salad",
-            "decription": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-            "price": "16,90€"
+            "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+            "price": "16,90",
+            "source": "./assets/img/warm_beef_arugula_salad.svg"
             },
             {
             "dish": "Mini Green Salad",
-            "decription": "Green salad, Cucumber, Carrots, Parsley, Radishes",
-            "price": "7,90€"
+            "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+            "price": "7,90",
+            "source": "./assets/img/mini_green_salad.svg"
             },
             {
             "dish": "Green Salad with sea food",
-            "decription": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-            "price": "16,90€"
+            "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "price": "16,90",
+            "source": "./assets/img/green_salad_with_sea_food.svg"
             },
             {
             "dish": "Vegan green salad with tofu",
-            "decription": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-            "price": "14,90€"
+            "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "price": "14,90",
+            "source": "./assets/img/vegan_green_salad_with_tofu.svg"
             }
-        ]
-    } 
 ]
 
-let dishImages = [
-    "./assets/img/veggie_mushroom_black_burger.jpg",
-    "./assets/img/all_meat_burger.jpg",
-    "./assets/img/beef_red_burger",
-    "./assets/img/blg_chicken_burger.jpg",
-    "./assets/img/pizza_margherita.jpg",
-    "./assets/img/pizza_chorizo.jpg",
-    "./assets/img/pizza_funghi.jpg",
-    "./assets/img/pizza_quattro_fromaggi.jpg",
-    "./assets/img/warm_beef_arugula_salad.jpg",
-    "./assets/img/mini_green_salad.jpg",
-    "./assets/img/green_salad_with_sea_food.jpg",
-    "./assets/img/vegan_green_salad_with_tofu.jpg"
+
+let categoryDetails = [
+    {"title": "Burger and Sandwiches",
+    "source": "./assets/logos/icon_burger_sandwiches_category.svg",
+    "meals": burgerAndSandwiches},
+    {"title": "Pizza (30cm)",
+    "source": "./assets/logos/icon_pizza_category.svg",
+    "meals": pizza},
+    {"title": "Salads",
+    "source": "./assets/logos/icon_salad_category.svg",
+    "meals": salad}
 ]
